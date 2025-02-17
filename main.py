2992d6999044d7aebca10a595cae34f6734bbe2b
Ahmet Çağatay Savaşlı <cagatay.savasli@ozu.edu.tr>
2025-02-17 17:37:52 +0300

    Update main according to updates

--- main.py
+++ main.py
@@ -8,34 +8,9 @@ def main():
 
     st.markdown("---")
 
-    with st.expander("📖 How to Use LazyLoader-GDELT 🦥"):
-        app.how_to_use()
+    app.select_app()
 
-    st.markdown("# Date Range:")
-    app.get_dates()
 
-    st.markdown("---")
-
-    st.markdown("# Actor Code Filter:")
-
-    with st.expander("🔎 Country Codes Searcher 🌍"):
-        app.camoe_code_searcher()
-
-    st.subheader("Filter by Actor 1")
-    app.actor_buttons("actor1")
-
-    st.subheader("Filter by Actor 2")
-    app.actor_buttons("actor2")
-
-    st.markdown("---")
-
-    if st.button("Apply Actor Filter"):
-        app.actor_filter()
-
-    if st.button("Load Data"):
-        app.load_data()
-
-    app.download_data_button()
 
 
 if __name__ == "__main__":
